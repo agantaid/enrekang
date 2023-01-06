653fc9e3af745a5e0331dd88f0ecb8fdbb100817
suarminrais <suarmin.rais@gmail.com>
2023-01-06 08:20:17 +0800

    fix: view implement

--- src/components/explore/relatedWisata.jsx
+++ src/components/explore/relatedWisata.jsx
@@ -1,12 +1,12 @@
 import { Box, Center, Container, Text, VStack } from '@chakra-ui/react';
 import SliderRelatedWisata from './SliderRelatedWisata';
 
-const RelatedWisata = () => {
+const RelatedWisata = ({ location }) => {
   return (
     <Box>
       <Container maxW="8xl">
         <Box px={{ md: '20px', xl: '142px' }} pb="78px">
-          <SliderRelatedWisata />
+          <SliderRelatedWisata location={location} />
         </Box>
       </Container>
     </Box>
